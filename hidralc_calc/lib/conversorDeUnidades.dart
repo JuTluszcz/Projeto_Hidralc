b931d2f075c73c352e2b50df1180d93cc160b143
JuTluszcz <juliatluszcz@alunos.utfpr.edu.br>
2022-03-23 19:08:29 -0300

    com drop

--- hidralc_calc/lib/conversorDeUnidades.dart
+++ hidralc_calc/lib/conversorDeUnidades.dart
@@ -2,19 +2,53 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class ConversorDeUnidades extends StatelessWidget {
+  get children => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Conversor de Unidades"),
+        title: const Text("Conversor de Unidades"),
       ),
-      body: Center(
-        child: BackButton(
-          onPressed: (){Navigator.pop(context);},
+      body: const ConversorForm(),
+      );
+  }
+}
+
+class ConversorForm extends StatefulWidget {
+  const ConversorForm({Key? key}) : super(key: key);
+
+  @override
+  State<ConversorForm> createState() => _ConversorFormState();
+}
+
+class _ConversorFormState extends State<ConversorForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build (BuildContext context){
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            validator: (value){
+              if (value == null || value.isEmpty){
+                return 'Insira um valor';
+              }
+              return null;
+            }
+          )
+         ],
         ),
-      ),
     );
   }
 }
+
+/*class Conversordearea extends StatelessWidget{
+
+  var areacolocada 
+} */
