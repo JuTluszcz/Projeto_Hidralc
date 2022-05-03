614f3bb133d64bd3c13927d476aadb912fbf32ee
JuTluszcz <juliatluszcz@alunos.utfpr.edu.br>
2022-05-03 13:50:17 -0300

    03/05 segunda

--- hidralc_calc/lib/propriedadesFisicasFluidos.dart
+++ hidralc_calc/lib/propriedadesFisicasFluidos.dart
@@ -7,13 +7,48 @@ class PropriedadesFisicasDosFluidos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Propriedades Fisicas dos Fluidos"),
+        title: const Text("Propriedades Fisicas dos Fluidos"),
       ),
-      body: Center(
-        child: BackButton(
-          onPressed: (){Navigator.pop(context);},
-        ),
-      ),
+      body: const ListPropFisicas(),
+    );
+  }
+}
+
+class ListPropFisicas extends StatefulWidget{
+  const ListPropFisicas({Key? key}) : super (key: key);
+  @override
+  _ListPropFisicas createState()=> _ListPropFisicas();
+}
+
+class _ListPropFisicas extends State<ListPropFisicas> {
+  @override 
+  Widget build (BuildContext context){
+    return MaterialApp(
+      home: ListView(
+        children: const [
+          Card(
+            child:ListTile(
+              title: Text("Propriedades da água"),
+            )
+          ),
+          Card(
+            child:ListTile(
+              title: Text("Densidades Relativas Usuais"),
+            )
+          ),
+          Card(
+            child:ListTile(
+              title:Text("Tensão superficial usual")
+            )
+          ),
+          Card(
+            child:ListTile(
+              title: Text("Capilaridade")
+            )
+          ),
+        ],
+        shrinkWrap: true,
+      )
     );
   }
 }
